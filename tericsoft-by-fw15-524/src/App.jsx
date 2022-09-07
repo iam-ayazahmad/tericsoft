b1aa96970fab8982fd02234f25da69900334e86e
Ayaz Ahmad <97458144+iam-ayazahmad@users.noreply.github.com>
2022-09-07 15:48:59 +0530

    new

---
+++ tericsoft-by-fw15-524/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Navbar } from './Components/Navbar'
+
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <img src="https://placekitten.com/360/360" alt="" /><img src="https://placekitten.com/360/360" alt="" /><img src="https://placekitten.com/360/360" alt="" />
+
+      
+      
+        
+      
+    </div>
+  )
+}
+
+export default App
---
+++ tericsoft-by-fw15-524/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "../Components/Navbar.css"
+
+export const Navbar=()=>{
+
+    return(
+        <div className="mainnav" style={{display :"flex"}}>
+
+            <div>home</div>
+            <div>new</div>
+            <div>photo</div>
+            <div>video</div>
+            <div>data</div>
+            
+        </div>
+    )
+}
